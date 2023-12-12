1864925bd626a8f78def92ae5f79126b21f39326
myunus-info <myunus_info@yahoo.com>
2023-12-12 13:07:42 +0600

    Project setup, header, footer, context, hook done

---
+++ src/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../Layout/Layout';
+
+const Home = () => {
+  return (
+    <Layout>
+      <h1>This is homepage</h1>
+    </Layout>
+  );
+};
+
+export default Home;
